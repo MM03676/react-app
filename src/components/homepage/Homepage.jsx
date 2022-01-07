2b27e747fd6f5a3122d84986e5e21bf7b2b26aa5
Milos <m.miljkovic@frontdeskanywhere.com>
2022-01-07 18:43:06 +0100

    Homepage restyling

--- src/components/homepage/Homepage.jsx
+++ src/components/homepage/Homepage.jsx
@@ -4,7 +4,7 @@ import './homepage.scss';
 const Homepage = () => {
     return (
         <div className='homepage'>
-            <div className="directory-meny">
+            <div className="directory-menu">
                 <div className="menu-item">
                     <div className="content">
                         <h1 className="title">Hats</h1>
